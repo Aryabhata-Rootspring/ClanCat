d314eaf93e9b5b120ad6b28d4ab67422a2be4915
Rootspring <rootspring@Rootsprings-MacBook.local>
2020-12-31 18:46:54 +0530

    Make server.py linty

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
-import asyncio
 import builtins
 import importlib
 import os
 from modules.db import setup_db
 
-app = FastAPI(root_path="/api/v1", servers=[{"url": "/api/v1", "description": "Backend API"}])
+db = builtins.db
+
+app = FastAPI(
+    root_path="/api/v1",
+    servers=[{"url": "/api/v1", "description": "Backend API"}]
+)
+
 
 @app.on_event("startup")
 async def startup():
     print("SERVER: Setting up database")
     builtins.db = await setup_db()
+
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -21,9 +27,11 @@ print("SERVER: Loading Modules")
 # Include all the modules
 for f in os.listdir("modules/server"):
     if not f.startswith("_"):
-        print("SERVER MODLOAD: modules.server." + f.replace(".py", ""))
-        route = importlib.import_module("modules.server." + f.replace(".py", ""))
+        module = "modules.server." + f.replace(".py", "")
+        print("SERVER MODLOAD: " + module)
+        route = importlib.import_module(module)
         app.include_router(route.router)
+
 
 @app.get("/")
 async def root():
